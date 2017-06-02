d7b8200b185bad027cc90b8741b51d8d36f22579
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-02 11:27:22 +0530

    GTCR-58 - Basic NodeJs application

---
+++ modules/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
+app.enable('trust proxy');
+app.set('port', process.env.PORT || 5000);
+app.use('/', express.static(__dirname + '/www'));
+app.use(bodyParser.urlencoded({extended: true}));    
+
+app.post('/contact', (req, res) => {
+	console.log('-------', here);
+}); 
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ modules/modules/contact.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let auth = ''
+let force = require("./force"),
+let jsforce = require('jsforce')
+
+exports.execute = (req, res) => {
+	console.log('execute');
+}	
