428a87675d8f28e70b77148c66ffd2e4970954f1
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-03 18:33:28 +0530

    updatedSalesforceConnection

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ let bodyParser = require('body-parser');
 let connection = {};
 
 if(!SF_REFRESH_TOKEN || !SF_ACCESS_TOKEN) {
-	/*let aouth2 = new sf.OAuth2({
+	
+	let aouth2 = new sf.OAuth2({
 		clientId: SF_CLIENT_ID,
 		clientSecret: SF_CLIENT_SECRET,
 		redirectUri : ''
@@ -29,6 +30,7 @@ if(!SF_REFRESH_TOKEN || !SF_ACCESS_TOKEN) {
 
 	app.get('/oauth2/callback', function(req, res) {
 	  let conn = new sf.Connection({ oauth2 : oauth2 });
+	  connection = conn;
 	  let code = req.param('code');
   		conn.authorize(code, function(err, userInfo) {
     		if (err) { 
@@ -37,13 +39,7 @@ if(!SF_REFRESH_TOKEN || !SF_ACCESS_TOKEN) {
     		process.env.SF_REFRESH_TOKEN  = conn.refreshToken;
 			process.env.SF_ACCESS_TOKEN = conn.refreshToken;    
 	  	});
-	});*/
-	let conn = new jsforce.connection({
-
 	});
-	conn.login(SF_USER_NAME, SF_PASSWORD, function(err, userInfo){
-		
-	})
 } else {
 	let connection = new jsforce.Connection({
 		oauth2: {
@@ -56,21 +52,19 @@ if(!SF_REFRESH_TOKEN || !SF_ACCESS_TOKEN) {
 		refreshToken: SF_REFRESH_TOKEN
 	});
 	connection.on('refresh', function(accessToken, res) {
-		console.log('res', res)
 		process.env.SF_REFRESH_TOKEN = res;
 		process.env.SF_ACCESS_TOKEN = accessToken;
 	});
 }
- 
-console.log('tgggggggggggggggggg')
 
 app.enable('trust proxy');
 app.set('port', process.env.PORT || 5000);
 app.use('/', express.static(__dirname + '/www'));
 app.use(bodyParser.urlencoded({extended: true}));    
 
-app.get('/', function(req, res){
-
+app.post('/contact', function(req, res){
+	var query = [];
+	/*conn.query("Select Id, Name from Contact where Name Like '%" + req.)*/
 }); 
 
 app.listen(app.get('port'), function () {
