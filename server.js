9a0519e936fcbb8c1c65759723ea67edca41ebb7
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-05 01:19:50 +0530

    oAuth minor fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('port', process.env.PORT || 5000);
 app.use('/', express.static(__dirname + '/www'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/contact', function(req, res) => {
+app.post('/contact', function(req, res) {
 	res.send({text: 'successful'});
 });
 
