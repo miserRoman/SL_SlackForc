795559d71b82e18b4203cee490788090c5c333aa
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-05 01:17:50 +0530

    Added oAuth

--- modules/salesforceOauth.js
+++ modules/salesforceOauth.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*let SLACK_LOGIN_TOKEN = process.env.SLACK_LOGIN_TOKEN;
+let SLACK_LOGIN_TOKEN = process.env.SLACK_LOGIN_TOKEN;
 let SLACK_LOGOUT_TOKEN = process.env.SLACK_LOGOUT_TOKEN;
 let SF_CLIENT_ID = process.env.SF_CLIENT_ID;
 let SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET;
@@ -23,4 +23,3 @@ let connection = nforce.createConnection({
 });
 
 exports.salesforceConnection = connection
-*/
