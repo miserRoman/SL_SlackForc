8684a3664472b3165e4dfe95a05ed8e184d4544b
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-05 00:05:51 +0530

    Updated package names

--- server.js
+++ server.js
@@ -1,14 +1,134 @@
-const http = require('http');
+"use strict";
 
-const hostname = '127.0.0.1';
-const port = 3000;
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.set('port', process.env.PORT || 5000);
+
+app.post('/contact', function(req, res) {
+	res.send({text: 'successful'});
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 });
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port 3000!')
+});
+
+/*app.enable('trust proxy');*/
+/*app.use('/', express.static(__dirname + '/www'));
+app.use(bodyParser.urlencoded({extended: true}));    
+*/
+
+
+/*
+let SLACK_LOGIN_TOKEN = process.env.SLACK_LOGIN_TOKEN;
+let SLACK_LOGOUT_TOKEN = process.env.SLACK_LOGOUT_TOKEN;
+let SF_CLIENT_ID = process.env.SF_CLIENT_ID;
+let SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET;
+let SF_LOGIN_URL = process.env.SF_LOGIN_URL;
+let SF_REFRESH_TOKEN = process.env.SF_REFRESH_TOKEN;
+let SF_ACCESS_TOKEN = process.env.SF_ACCESS_TOKEN;
+let SF_USER_NAME = process.env.SF_USER_NAME;
+let SF_PASSWORD = process.env.SF_PASSWORD;
+*/
+
+/*let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+*/
+/*let org = require('./modules/salesforceOauth').salesforceConnection
+*/
+/*
+let connection = {};
+
+if(!SF_REFRESH_TOKEN || !SF_ACCESS_TOKEN) {
+	
+	let aouth2 = new sf.OAuth2({
+		clientId: SF_CLIENT_ID,
+		clientSecret: SF_CLIENT_SECRET,
+		redirectUri : ''
+	});
+
+	app.get('/oauth2/auth', function(req, res){
+		res.redirect(aouth2.getAuthorizationUrl({scope: ''}))
+	});
+
+	app.get('/oauth2/callback', function(req, res) {
+	  let conn = new sf.Connection({ oauth2 : oauth2 });
+	  connection = conn;
+	  let code = req.param('code');
+  		conn.authorize(code, function(err, userInfo) {
+    		if (err) { 
+    			return console.error(err); 
+    		}
+    		process.env.SF_REFRESH_TOKEN  = conn.refreshToken;
+			process.env.SF_ACCESS_TOKEN = conn.refreshToken;    
+	  	});
+	});
+} else {
+	let conn = new jsforce.Connection({
+		oauth2: {
+			clientId: SF_CLIENT_ID,
+			clientSecret: SF_CLIENT_SECRET,
+			redirectUri: ''
+		},
+		instanceUrl: SF_LOGIN_URL,
+		accessToken: SF_ACCESS_TOKEN, 
+		refreshToken: SF_REFRESH_TOKEN
+	});
+	conn.on('refresh', function(accessToken, res) {
+		process.env.SF_REFRESH_TOKEN = res;
+		process.env.SF_ACCESS_TOKEN = accessToken;
+	});
+	connection = conn;
+}*/
+
+/*app.enable('trust proxy');
+app.set('port', process.env.PORT || 5000);
+app.use('/', express.static(__dirname + '/www'));
+app.use(bodyParser.urlencoded({extended: true}));    
+*/
+/*app.post('/contact', function(req, res){
+	var records = [];
+	var query = connection.query("Select Id, Name from Contact where Name Like '%" + req.body.text + "%'")
+			   .on('record',function(record){
+			   		records.push(record);
+				})
+			   .on('end', function(){
+			   		console.log('query', query.totalSize);
+			   		console.log('query', query.totalFetched);
+			   })
+			   .on('err', function(){
+			   		console.log('err', err);
+			   })
+			   .run({
+			   		autoFetch: true,
+			   		maxFetch: 4000
+			   })
+	console.log('dddddddd', req.body.text);		   
+}); */
+
+/*app.post('/contact', function(req, resp) {
+	resp.send({text: 'Testing connection'});
+	org.query(
+		{query: "Select Id, Name from Contact where Name Like '%" + req.body.text + "%'"},
+		function(err, records) {
+			if(err) throw err;
+			else {
+				console.log('query completed with token: ' + oauth.access_token); // <--- if refreshed, this should be different
+    			res.send(body);
+  			}
+		});
+});*/
+
+/*app.post('/contact', (req, res) => {
+	res.send({text: 'successful'});
+})
+
+app.listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
+});*/
