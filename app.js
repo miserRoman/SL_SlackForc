3724697d19cd4639a18ba44c1ac940af8ced6e5e
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-04 20:52:47 +0530

    basic connection

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 
-app.enable('trust proxy');
-/*app.set('port', process.env.PORT || 5000);*/
+/*app.enable('trust proxy');
+app.set('port', process.env.PORT || 5000);
 app.use('/', express.static(__dirname + '/www'));
 app.use(bodyParser.urlencoded({extended: true}));    
+*/
 
 app.post('/contact', (req, res) => {
 	res.send({text: 'successful'});
 })
 
-app.listen(app.get('port'), function () {
+/*app.listen(app.get('port'), function () {
 	console.log('Express server listening on port ' + app.get('port'));
-});
+});*/
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
 
 /*
 let SLACK_LOGIN_TOKEN = process.env.SLACK_LOGIN_TOKEN;
