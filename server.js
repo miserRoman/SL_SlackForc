311e343087cb0dff44a3dbe1f1db25de1fea4d18
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-05 20:13:50 +0530

    Added oAuth2

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/login', function(req, res){
 		loginUrl : SF_LOGIN_URL,
 	    clientId: SF_CLIENT_ID,
 	    clientSecret: SF_CLIENT_SECRET,
-	    redirectUri: 'https://${req.hostname}/login'
+	    redirectUri: 'https://salesforce-slack-connect.herokuapp.com/login'
 	});
 	
 	slackConnections[req.user_id] = {};
