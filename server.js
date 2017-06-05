5a89a4a4b5185ff620581ea46ba37b4cb2f21cce
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-05 17:54:04 +0530

    Added oAuth2

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/login', function(req, res){
 	
-	var oauth2 = new jsforce.OAuth2({
+	let oauth2 = new jsforce.OAuth2({
+		loginUrl : SF_LOGIN_URL,
 	    clientId: SF_CLIENT_ID,
 	    clientSecret: SF_CLIENT_SECRET,
 	    redirectUri: ''
