fd22a062754b1af4cd3f1fed1834de67e6cd4116
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-06 00:55:18 +0530

    testing

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use('/', express.static(__dirname + '/www'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/login', function(req, res){
-	slackConnections[req.params.user_id] = {};
-	console.log('sssss', req)
+	slackConnections[req.query.user_id] = {};
+	console.log('sssss', req.query.user_id)
 	res.redirect(`${SF_LOGIN_URL}/services/oauth2/authorize?response_type=code&client_id=${SF_CLIENT_ID}&redirect_uri=https://${req.hostname}/oauthcallback&state=${req.params.user_id}`);
 });
 
