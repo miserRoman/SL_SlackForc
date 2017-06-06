f6c545eb2614d533de1357c2c36617d7db6d348c
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-06 11:59:24 +0530

    testing

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/login', function(req, res){
 	let redirectURL = `${SF_LOGIN_URL}/services/oauth2/authorize?response_type=code&client_id=${SF_CLIENT_ID}&redirect_uri=https://salesforce-slack-connect.herokuapp.com/oauthcallback&state=${req.query.user_id}`;
+	console.log('ssssss', redirectURL);
 	res.redirect(redirectURL);
 });
 
