55ae54954e080283a598f03f61feacb5979c6c3c
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-06 10:55:30 +0530

    testing

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/login', function(req, res){
 	slackConnections[req.query.user_id] = {};
 	console.log('sssss', req.query.user_id)
+	console.log('login', SF_LOGIN_URL);
+	console.log('Client id', SF_CLIENT_ID);
 	res.redirect(`${SF_LOGIN_URL}/services/oauth2/authorize?response_type=code&client_id=${SF_CLIENT_ID}&redirect_uri=https://salesforce-slack-connect.herokuapp.com/oauthcallback&state=${req.query.user_id}`);
 });
 
@@ -73,8 +75,8 @@ app.post('/contact', function(req, res) {
 	  	    	autoFetch : true, 
 	  	    	maxFetch : 4000 
 	  	    });
-	});
-*/
+	});*/
+
 });
 
 app.listen(app.get('port'), function () {
