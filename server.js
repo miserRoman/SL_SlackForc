4081a88fca5cb6318a3c7ac5594ffd449c7a679a
Nandish Shenoy <nandish.shenoy@silverlinecrm.com>
2017-06-07 00:58:58 +0530

    test

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ let app = express();
 let bodyParser = require('body-parser');
 let jsforce = require('jsforce');
 let request = require('request');
-/*let salesforce = require('salesforceOauth');*/
 
 let slackConnections = {};
 
@@ -38,7 +37,18 @@ app.get('/login/:slackUserId', function(req, res){
 
 app.get('/oauthcallback', function(req, res){
 	let slackUserId = req.query.state;
-	let options = {
+	var conn = new sf.Connection({ oauth2 : oauth2 });
+	var code = req.param('code');
+	conn.authorize(code, function(err, userInfo) {
+		if (err) { return console.error(err); }
+		slackConnections[slackUserId] = conn;
+		console.log(conn.accessToken);
+		console.log(conn.refreshToken);
+		console.log(conn.instanceUrl);
+		console.log("User ID: " + userInfo.id);
+		console.log("Org ID: " + userInfo.organizationId);
+	});
+	/*let options = {
         url: `${SF_LOGIN_URL}/services/oauth2/token`,
         qs: {
             grant_type: "authorization_code",
@@ -70,7 +80,7 @@ app.get('/oauthcallback', function(req, res){
             </html>
             `;
         res.send(html);
-    });
+    });*/
 });
 
 app.post('/contact', function(req, res) {
@@ -141,115 +151,3 @@ app.post('/contact', function(req, res) {
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!')
 });
-
-/*let sfrequest = (oauth, path, options) => new Promise((resolve, reject) => {
-
-    if (!oauth || (!oauth.access_token && !oauth.refresh_token)) {
-        reject({code: 401});
-        return;
-    }
-
-    options = options || {};
-
-    options.method = options.method || 'GET';
-
-    if (path.charAt(0) !== '/') {
-        path = '/' + options.path;
-    }
-
-    options.url = oauth.instance_url + path;
-
-    options.headers = options.headers || {};
-
-    options.headers["Accept"]= "application/json";
-    options.headers["Authorization"] = "Bearer " + oauth.access_token;
-
-    request(options, function (error, response, body) {
-        if (error) {
-            console.log(error);
-            if (response.statusCode === 401) {
-                // Could implement refresh token and retry logic here
-                reject({code: 401});
-            } else {
-                reject(error);
-            }
-        } else {
-            resolve(body);
-        }
-    });
-
-});
-*/
-
-
-
-
-/*
-let connection = {};
-
-if(!SF_REFRESH_TOKEN || !SF_ACCESS_TOKEN) {
-	
-	let aouth2 = new sf.OAuth2({
-		clientId: SF_CLIENT_ID,
-		clientSecret: SF_CLIENT_SECRET,
-		redirectUri : ''
-	});
-
-	app.get('/oauth2/auth', function(req, res){
-		res.redirect(aouth2.getAuthorizationUrl({scope: ''}))
-	});
-
-	app.get('/oauth2/callback', function(req, res) {
-	  let conn = new sf.Connection({ oauth2 : oauth2 });
-	  connection = conn;
-	  let code = req.param('code');
-  		conn.authorize(code, function(err, userInfo) {
-    		if (err) { 
-    			return console.error(err); 
-    		}
-    		process.env.SF_REFRESH_TOKEN  = conn.refreshToken;
-			process.env.SF_ACCESS_TOKEN = conn.refreshToken;    
-	  	});
-	});
-} else {
-	let conn = new jsforce.Connection({
-		oauth2: {
-			clientId: SF_CLIENT_ID,
-			clientSecret: SF_CLIENT_SECRET,
-			redirectUri: ''
-		},
-		instanceUrl: SF_LOGIN_URL,
-		accessToken: SF_ACCESS_TOKEN, 
-		refreshToken: SF_REFRESH_TOKEN
-	});
-	conn.on('refresh', function(accessToken, res) {
-		process.env.SF_REFRESH_TOKEN = res;
-		process.env.SF_ACCESS_TOKEN = accessToken;
-	});
-	connection = conn;
-}*/
-
-/*app.enable('trust proxy');
-app.set('port', process.env.PORT || 5000);
-app.use('/', express.static(__dirname + '/www'));
-app.use(bodyParser.urlencoded({extended: true}));    
-*/
-/*app.post('/contact', function(req, res){
-	var records = [];
-	var query = connection.query("Select Id, Name from Contact where Name Like '%" + req.body.text + "%'")
-			   .on('record',function(record){
-			   		records.push(record);
-				})
-			   .on('end', function(){
-			   		console.log('query', query.totalSize);
-			   		console.log('query', query.totalFetched);
-			   })
-			   .on('err', function(){
-			   		console.log('err', err);
-			   })
-			   .run({
-			   		autoFetch: true,
-			   		maxFetch: 4000
-			   })
-	console.log('dddddddd', req.body.text);		   
-}); */
